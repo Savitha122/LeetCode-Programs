9729155126897d36781132ca413c1b345cce99ca
Savitha122 <167208018+Savitha122@users.noreply.github.com>
2026-02-23 06:51:09 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (42.58%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.c
@@ -0,0 +1,33 @@
+bool isValid(char* s) {
+    char stack[100000];
+    int top=-1;
+    for(int i=0;s[i]!='\0';i++)
+    {
+        char ch=s[i];
+        if(ch=='('||ch=='{'||ch=='[')
+        {
+           stack[++top]=ch;
+        }
+        else
+        {
+              if(top==-1)
+              return false;
+              char last =stack[top--];
+              if((ch==')'&&last!='(')||
+              (ch=='}'&&last!='{')||
+              (ch==']'&&last!='['))
+              {
+                  return false;
+              }
+        }
+    }
+          return top==-1;
+}
+   
+            
+            
+           
+        
+
+  
+    

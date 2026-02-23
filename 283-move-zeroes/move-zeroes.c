c62482719e47daff91762c115786d33d0dca8d0a
Savitha122 <167208018+Savitha122@users.noreply.github.com>
2026-02-23 08:59:52 +0530

    Time: 2 ms (18.46%) | Memory: 19.6 MB (88.45%) - LeetSync

--- 283-move-zeroes/move-zeroes.c
+++ 283-move-zeroes/move-zeroes.c
@@ -1,17 +1,16 @@
 void moveZeroes(int* nums, int numsSize) {
     int index=0;
-    for (int i = 0; i < numsSize; i++)
-     {
-        
-        if (nums[i] != 0) 
+    for(int i=0;i<numsSize;i++)
+    {
+        if(nums[i]!=0)
         {
-            nums[index] = nums[i];
+            nums[index]=nums[i];
             index++;
         }
     }
-    while(index < numsSize) 
+    while(index<numsSize)
     {
-        nums[index]= 0;
+        nums[index]=0;
         index++;
     }
     return ;

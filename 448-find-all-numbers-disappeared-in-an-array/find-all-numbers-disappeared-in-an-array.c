fc012960e99e4b017cbd1f23af632f59edf8d613
Savitha122 <167208018+Savitha122@users.noreply.github.com>
2026-03-03 21:46:41 +0530

    Time: 0 ms (100.00%) | Memory: 36.1 MB (39.95%) - LeetSync

---
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <math.h>
+
+int* findDisappearedNumbers(int* nums, int numsSize, int* returnSize) {
+    for(int i = 0; i < numsSize; i++) {
+        int index = abs(nums[i]) - 1;
+        
+        if(nums[index] > 0) {
+            nums[index] = -nums[index];
+        }
+    }
+    int* result = (int*)malloc(numsSize * sizeof(int));
+    int count = 0;
+
+    for(int i = 0; i < numsSize; i++) {
+        if(nums[i] > 0) {
+            result[count] = i + 1;
+            count++;
+        }
+    }
+
+    *returnSize = count;
+    return result;
+}

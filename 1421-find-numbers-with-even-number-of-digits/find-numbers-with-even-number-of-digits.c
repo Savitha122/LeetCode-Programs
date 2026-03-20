0dde4c1f3d6f50add2c3a5c6a46df66dfc2b1b89
Savitha122 <167208018+Savitha122@users.noreply.github.com>
2026-03-20 10:18:08 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (27.06%) - LeetSync

---
+++ 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.c
@@ -0,0 +1,20 @@
+int findNumbers(int* nums, int numsSize) {
+    int count=0;
+    for(int i=0;i<numsSize;i++)
+    {
+        int num=nums[i];
+        int digits=0;
+        if(num==0)
+        digits=1;
+        while(num!=0)
+        {
+            num/=10;
+            digits++;
+        }
+        if(digits%2==0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
